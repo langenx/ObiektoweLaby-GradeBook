d80eae2922bab54ee9e53dd48b2ab083eb255907
Langen <langen107@gmail.com>
2024-03-12 20:33:15 +0100

    Added a new public property called Type of BasedGradeBook type

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -266,5 +266,7 @@ namespace GradeBook.GradeBooks
             
             return JsonConvert.DeserializeObject(json, gradebook);
         }
+
+        public GradeBookType Type { get; set; }
     }
 }
