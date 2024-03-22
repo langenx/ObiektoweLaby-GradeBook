d36e212cb7b45540edcd06d6ae4bb70d94652260
Langen <langen107@gmail.com>
2024-03-22 13:35:17 +0100

    Updated BaseGradeBook.GetGPA by adding 1 point when student is Honors or DualEnrolled

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -108,18 +108,23 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            int point = 0;
+            if(studentType == StudentType.Honors || studentType == StudentType.DualEnrolled)
+            {
+                point = 1;
+            }
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    return 4 + point;
                 case 'B':
-                    return 3;
+                    return 3 + point;
                 case 'C':
-                    return 2;
+                    return 2 + point;
                 case 'D':
-                    return 1;
+                    return 1 + point;
                 case 'F':
-                    return 0;
+                    return 0 + point;
             }
             return 0;
         }
